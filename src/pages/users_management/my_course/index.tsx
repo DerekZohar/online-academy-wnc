d243babfc36ca034792311684cd73cd9add999a1
Nguyễn Hữu Tuấn <huutuan345679@gmail.com>
2021-01-09 14:06:34 +0700

    custom my course

--- src/pages/users_management/my_course/index.tsx
+++ src/pages/users_management/my_course/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import './styles.css'
-import Course from '../../../components/course';
+import MyCourse from '../../../components/my-course';
 
 
-export default function MyCourse() {
+export default function MyLearning() {
         const course = {
 		imgUrl:
 			'https://img-a.udemycdn.com/course/240x135/950390_270f_3.jpg?yytG3JZ7tYO72YREFLuJZFmzcVBR1tht6FhV7r20dE1cYJI0EmPQGP5Q_UJXbjnNVdrVKCFtylxND-SIeZhKp5viPP0VHThFLzlO98p1Fc1J9rpF58mDvLro--5NB3Y',
@@ -12,16 +12,14 @@ export default function MyCourse() {
 			'Learn Python like a Professional Start from the basics and go all the way to creating your own applications and games',
 		authors: 'NVT',
 		people: 137190,
-		rating: 4.6,
-		price: 120,
-		discount: 0.8
+		progress: 70,
 	};
 	return (
 		<div className="Watchlist-Container">
 			<h2>WatchList</h2>
 			<div className='Course-Container'>
-				<Course {...course} />
-				<Course {...course} />
+				<MyCourse {...course} />
+				<MyCourse {...course} />
 			</div>
 		</div>
 	);
