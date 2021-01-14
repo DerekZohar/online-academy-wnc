fe7989262c23ba1d7fa8ad9704f1445743986b0e
Nguyễn Hữu Tuấn <huutuan345679@gmail.com>
2021-01-14 14:30:44 +0700

    complete admin category

--- src/App.tsx
+++ src/App.tsx
@@ -1,77 +1,74 @@
-import React from "react";
-import "./App.css";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  useHistory,
-} from "react-router-dom";
-import LoginPage from "./pages/login";
-import firebase from "firebase";
-import { firebaseConfig } from "./services/firebase";
+import React from 'react';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route, useHistory } from 'react-router-dom';
+import LoginPage from './pages/login';
+import firebase from 'firebase';
+import { firebaseConfig } from './services/firebase';
 // import WebCategoryPage from "./pages/categories/item";
-import CategoryDetails from "./pages/categories";
-import HomePage from "./pages/home";
-import Navbar from "./components/home-details/navbar";
-import PageNotFound from "./pages/page-not-found";
-import CourseDetail from "./components/course-details";
-import ScrollToTop from "./components/scroll-to-top";
-import { checkObjEmpty } from "./helpers/checkObjEmpty";
-import { userLogin } from "./pages/login/loginSlice";
-import { useDispatch } from "react-redux";
-import LearnCourse from "./pages/learn-course";
-import WatchList from "./pages/watch-list";
-import Profile from "./pages/profile";
-import CourseManagement from './pages/admin/course-management'
+import CategoryDetails from './pages/categories';
+import HomePage from './pages/home';
+import Navbar from './components/home-details/navbar';
+import PageNotFound from './pages/page-not-found';
+import CourseDetail from './components/course-details';
+import ScrollToTop from './components/scroll-to-top';
+import { checkObjEmpty } from './helpers/checkObjEmpty';
+import { userLogin } from './pages/login/loginSlice';
+import { useDispatch } from 'react-redux';
+import LearnCourse from './pages/learn-course';
+import WatchList from './pages/watch-list';
+import Profile from './pages/profile';
+import CourseManagement from './pages/admin/course-management';
+import CategoryManagement from './pages/admin/categories-management'
 
 function App() {
-  if (!firebase.apps.length) {
-    firebase.initializeApp(firebaseConfig);
-  } else {
-    firebase.app(); // if already initialized, use that one
-  }
-  const history = useHistory();
-  const userInfo = localStorage.getItem("userInfo")
-    ? localStorage.getItem("userInfo")
-    : "";
-  const dispatch = useDispatch();
+	if (!firebase.apps.length) {
+		firebase.initializeApp(firebaseConfig);
+	} else {
+		firebase.app(); // if already initialized, use that one
+	}
+	const history = useHistory();
+	const userInfo = localStorage.getItem('userInfo') ? localStorage.getItem('userInfo') : '';
+	const dispatch = useDispatch();
 
-  console.log(checkObjEmpty(userInfo));
-  if (checkObjEmpty(userInfo) === false) {
-    dispatch(userLogin(JSON.parse(userInfo || "{}")));
-    // history.push("/");
-  }
-  return (
-    <Router>
-      <ScrollToTop />
-      <Navbar />
-      <Switch>
-        <Route exact path="/">
-          <HomePage />
-        </Route>
-        <Route path="/login">
-          <LoginPage login={true} />
-        </Route>
-        <Route path="/sign-out">
-          <LoginPage login={false} />
-        </Route>
-        <Route path={`/category/web/:webId`}>
-          <CourseDetail />
-        </Route>
-        <Route path={`/category/:categoryName`}>
-          <CategoryDetails />
-        </Route>
+	console.log(checkObjEmpty(userInfo));
+	if (checkObjEmpty(userInfo) === false) {
+		dispatch(userLogin(JSON.parse(userInfo || '{}')));
+		// history.push("/");
+	}
+	return (
+		<Router>
+			<ScrollToTop />
+			<Navbar />
+			<Switch>
+				<Route exact path="/">
+					<HomePage />
+				</Route>
+				<Route path="/login">
+					<LoginPage login={true} />
+				</Route>
+				<Route path="/sign-out">
+					<LoginPage login={false} />
+				</Route>
+				<Route path={`/category/web/:webId`}>
+					<CourseDetail />
+				</Route>
+				<Route path={`/category/:categoryName`}>
+					<CategoryDetails />
+				</Route>
 
-        <Route path={"/user/learning"} component={LearnCourse} />
-        <Route path={"/user/watchlist"} component={WatchList} />
-        <Route path={"/user/profile"} component={Profile} />
-        <Route path={'/admin/course-management'}>
-          <CourseManagement/>
+				<Route path={'/user/learning'} component={LearnCourse} />
+				<Route path={'/user/watchlist'} component={WatchList} />
+				<Route path={'/user/profile'} component={Profile} />
+				<Route path={'/admin/course-management'}>
+					<CourseManagement />
         </Route>
-        <Route component={PageNotFound} />
-      </Switch>
-    </Router>
-  );
+        <Route path={'/admin/categories-management'}>
+          <CategoryManagement/>
+        </Route>
+				<Route component={PageNotFound} />
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
